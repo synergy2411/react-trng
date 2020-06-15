e1dc9f3baa933394b789a9427128bcf9c6bb2026
unknown <synergy2411@outlook.com>
2020-06-15 12:19:06 +0530

    first commit

--- playground/destructuring.js
+++ playground/destructuring.js
@@ -6,7 +6,6 @@
 // console.log(f3);            // Bas
 // console.log(f5);            // undefined
 
-
 // Object Destructuring
 // let draw = {
 //     drawCircle : r =>  console.log(Math.PI * r * r),
@@ -18,23 +17,31 @@
 // drawText("Sample Text");
 // dc(3);
 
-
-
-
 // Nested Destructuring
 
-let user ={
-    firstName : "Foo",
-    lastName : "Bar",
-    address :{
-        city : "Bengaluru",
-        street : "201, Main Road, Marathahalli"
-    },
-    friends : ["Baz", "Bam", "Bas"]
-}
+let user = {
+  firstName: "Foo",
+  lastName: "Bar",
+  address: {
+    city: "Bengaluru",
+    street: "201, Main Road, Marathahalli",
+  },
+  friends: ["Baz", "Bam", "Bas"],
+};
+
+let {
+  firstName: fn,
+  lastName: ln,
+  address: { city: ct, street: st },
+  friends: [f1, f2, f3],
+} = user;
+
+console.log(fn, ct, f2)
+
+
 
 let todos = [
-    {label : "plant", status : true},
-    {label : "shop", status : false},
-    {label : "read", status : true},
-]
+  { label: "plant", status: true },
+  { label: "shop", status: false },
+  { label: "read", status: true },
+];
