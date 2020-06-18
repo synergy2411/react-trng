c45728d13dff5e10f86f2fe837103fd9a35bc83f
unknown <synergy2411@outlook.com>
2020-06-18 13:09:00 +0530

    SPA Nested routing

--- basic-app/src/App.js
+++ basic-app/src/App.js
@@ -1,52 +1,50 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Note from './Container/Note/Note';
+import Note from "./Container/Note/Note";
 import LifeCycleDemo from "./Container/LifeCycle/LifeCycle";
 import Parent from "./Container/Props/Parent";
-import ControlledComponent from './Container/ControlledComponent/FormComponent';
+import ControlledComponent from "./Container/ControlledComponent/FormComponent";
 import FuncCmp from "./Container/Hooks/UseStateHook";
 
-import { BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom';
-
+import { BrowserRouter as Router, Link, Switch, Route, useRouteMatch, useParams } from "react-router-dom";
 
 function App() {
-
   return (
     <Router>
       <div className="container">
         <nav>
-        <ul className="list-unstyled">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/lifecycle">Life Cycle Demo</Link>
-          </li>
-          <li>
-            <Link to="/props">Props Demo</Link>
-          </li>
-          <li>
-            <Link to="/forms">Forms Demo</Link>
-          </li>
-          <li>
-            <Link to="/hooks">Hooks Demo</Link>
-          </li>
-          <li>
-            <Link to="/note">Note Demo</Link>
-          </li>
-        </ul>
+          <ul className="list-unstyled">
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/lifecycle">Life Cycle Demo</Link>
+            </li>
+            <li>
+              <Link to="/props">Props Demo</Link>
+            </li>
+            <li>
+              <Link to="/forms">Forms Demo</Link>
+            </li>
+            <li>
+              <Link to="/hooks">Hooks Demo</Link>
+            </li>
+            <li>
+              <Link to="/note">Note Demo</Link>
+            </li>
+          </ul>
         </nav>
       </div>
 
       <Switch>
-        <Route exact path="/">
+        <Route exact path="/home">
           <Home />
         </Route>
         <Route path="/note">
           <Note />
         </Route>
-        <Route path = "/lifecycle">
+        <Route path="/lifecycle">
           <LifeCycleDemo />
         </Route>
         <Route path="/props">
@@ -59,15 +57,50 @@ function App() {
           <FuncCmp />
         </Route>
       </Switch>
-
     </Router>
-
-   )
-   function Home() {
+  );
+  function Home() {
+    console.log("ROUTE MATCH : ",  useRouteMatch());
+    const {url, path } = useRouteMatch();
     return (
-      <h3>Home Component</h3>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-4">
+            <ul className="list-group">
+              <li className="list-group-item">
+                <Link to ={`${url}/javascript`}>JavaScript</Link>    
+              </li>
+              <li className="list-group-item">
+                <Link to ={`${url}/react`}>React</Link>
+              </li>
+              <li className="list-group-item">
+                <Link to ={`${url}/node`}>Node</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <Switch>
+          <Route exact path={path}>
+            <h3>Home Component Loaded....</h3>
+          </Route>
+          <Route path={`${path}/:courseId`}>
+            <Course />
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
+
+  function Course(){
+    const {courseId} = useParams();
+    console.log("Course Loaded", useParams());
+    return (
+      <div>
+        Course Component Loaded with {courseId}.....
+      </div>
     )
   }
+
   // function About(){
   //   return (
   //     <h3>About Component</h3>
@@ -80,7 +113,7 @@ function App() {
   // }
 
   // return (
-  //   // React.createElement("div", {className : "App"}, 
+  //   // React.createElement("div", {className : "App"},
   //   //   React.createElement("p", {}, "This app is working fine...") )
 
   //     <div className="container">
